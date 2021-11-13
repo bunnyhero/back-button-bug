06edc257f8598757979cdf42cba36576d67df137
bunnyhero <bunnyhero@bunnyhero.com>
2021-11-13 11:55:23 -0500

    Add docco comment

--- Back Button Bug/UIView+Extensions.swift
+++ Back Button Bug/UIView+Extensions.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 extension UIView {
+    /// Counts the total number of gesture recognizers owned by this UIView and all its subviews
+    /// - Returns: Total number of gesture recognizers
     func recursiveGestureRecognizerCount() -> Int {
         let myCount = gestureRecognizers?.count ?? 0
         return subviews.reduce(myCount, { $0 + $1.recursiveGestureRecognizerCount() })
