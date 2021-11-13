445bd3b55b22993ab7052a098fe67a28d6c6ff89
bunnyhero <bunnyhero@bunnyhero.com>
2021-11-12 20:00:05 -0500

    add views to reproduce bug

--- Back Button Bug/ViewController.swift
+++ Back Button Bug/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func runTest(_ sender: Any) {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: nil, action: nil)
+    }
 }
 
